0cec20fb53fed1d5bc30ae898301a5aa9c7af9b4
Jholvin <jhoelespro123@gmail.com>
2024-01-12 11:44:24 +0100

    Empezando con NODE

---
+++ NODE/mi-primera-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Evento from './components/Evento.js';
+import './styles/Evento.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Evento />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ NODE/mi-primera-app/src/components/Evento.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Evento() {
+    return (
+        <div className="card-container">
+
+            <img className="card-image"
+                src={require("../images/miku-icon.jpg")}
+                alt="foto del evento" />
+
+            <div className="card-content">
+                <h2 className="card-title">Py pizza 03 - PyPuzzle</h2>
+                <p className="card-date">09 de Diciembre de 2023 - 13:30 GMT-4</p>
+                <p className="card-place">Mr Pizza, 20 de Octubre Frente a la Plaza Abaroa</p>
+                <p className="card-description">Hola pythonista dheaaa
+                participa en este evento prescencial de la comunidad de python La Paz
+                porque puede y quieres y no se que mas esto ya es relleno xD
+                </p>
+
+            </div>    
+            
+                  
+        </div>
+
+        
+
+        
+
+        
+    );
+}
+
+export default Evento; 
